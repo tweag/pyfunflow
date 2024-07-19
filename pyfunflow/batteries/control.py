62f2bdca1b5446359ecdc5a35b32da34239a7965
Guillaume Desforges <guillaume.desforges.pro@gmail.com>
2024-07-19 12:34:48 +0200

    feat: conditional branching

--- pyfunflow/batteries/control.py
+++ pyfunflow/batteries/control.py
@@ -1,4 +1,4 @@
-from typing import Iterable
+from typing import Iterable, Union, cast
 from pyfunflow.core import Flow, ResultStore
 
 
@@ -15,3 +15,37 @@ class SequenceFlow[O](Flow[O]):
 
 def interpret_sequence_flow(result_store: ResultStore, flow: SequenceFlow):
     return result_store.get(flow.flows[-1])
+
+
+class BranchFlow[O1, O2](Flow[O1 | O2]):
+    def __init__(
+        self,
+        condition: Flow[bool],
+        flow_true: Flow[O1],
+        flow_false: Flow[O2],
+    ) -> None:
+        super().__init__(
+            cast(
+                type[O1 | O2],
+                Union[flow_true._output_type, flow_false._output_type],
+            ),
+        )
+        self.condition = condition
+        self.flow_true = flow_true
+        self.flow_false = flow_false
+
+    def __getsubflows__(self) -> Iterable[Flow]:
+        yield from self.condition.__getsubflows__()
+        yield from self.flow_true.__getsubflows__()
+        yield from self.flow_false.__getsubflows__()
+        yield self
+
+
+def interpret_branch_flow[O1, O2](
+    result_store: ResultStore,
+    flow: BranchFlow[O1, O2],
+) -> O1 | O2:
+    if result_store.get(flow.condition):
+        return result_store.get(flow.flow_true)
+    else:
+        return result_store.get(flow.flow_false)
--- tests/test_batteries.py
+++ tests/test_batteries.py
@@ -1,6 +1,11 @@
 from typing import Any, Callable, cast
 from pyfunflow.batteries import run_sequential_local
-from pyfunflow.batteries.control import SequenceFlow, interpret_sequence_flow
+from pyfunflow.batteries.control import (
+    BranchFlow,
+    SequenceFlow,
+    interpret_branch_flow,
+    interpret_sequence_flow,
+)
 from pyfunflow.core import Flow, RefBack, ResultStore
 
 
@@ -78,3 +83,89 @@ def test_run_local_sequential_sequence_output():
     # then
     EXPECTED = 3
     assert result == EXPECTED
+
+
+def test_run_local_branch():
+    """
+    Test that we can run a BranchFlow locally.
+    """
+
+    # given
+    class AddFlow(Flow[int]):
+        def __init__(self, x: int | RefBack[int, int], y: int) -> None:
+            self.x = x
+            self.y = y
+            super().__init__(int)
+
+    class IsEvenFlow(Flow[bool]):
+        def __init__(self, x: int | RefBack[int, int]) -> None:
+            self.x = x
+            super().__init__(bool)
+
+    flow12 = SequenceFlow(
+        flows=[
+            (
+                previous := SequenceFlow(
+                    flows=[],
+                    final_flow=AddFlow(2, 0),
+                )
+            )
+        ],
+        final_flow=BranchFlow(
+            condition=IsEvenFlow(previous.output),
+            flow_true=AddFlow(previous.output, 10),
+            flow_false=AddFlow(previous.output, 100),
+        ),
+    )
+
+    flow101 = SequenceFlow(
+        flows=[
+            (
+                previous := SequenceFlow(
+                    flows=[],
+                    final_flow=AddFlow(1, 0),
+                )
+            )
+        ],
+        final_flow=BranchFlow(
+            condition=IsEvenFlow(previous.output),
+            flow_true=AddFlow(previous.output, 10),
+            flow_false=AddFlow(previous.output, 100),
+        ),
+    )
+
+    def interpret_add(result_store: ResultStore, flow: AddFlow):
+        x = flow.x if isinstance(flow.x, int) else result_store.get(flow.x.back)
+        return x + flow.y
+
+    def interpret_is_even(result_store: ResultStore, flow: IsEvenFlow):
+        if isinstance(flow.x, int):
+            return flow.x % 2 == 0
+        else:
+            return flow.x.map_(result_store.get(flow.x.back)) % 2 == 0
+
+    def dispatcher[F](flow: F) -> Callable[[ResultStore, F], Any]:
+        if isinstance(flow, AddFlow):
+            return cast(Callable[[ResultStore, F], Any], interpret_add)
+        elif isinstance(flow, IsEvenFlow):
+            return cast(Callable[[ResultStore, F], Any], interpret_is_even)
+        elif isinstance(flow, SequenceFlow):
+            return cast(Callable[[ResultStore, F], Any], interpret_sequence_flow)
+        elif isinstance(flow, BranchFlow):
+            return cast(Callable[[ResultStore, F], Any], interpret_branch_flow)
+        else:
+            raise ValueError(f"Unknown flow type: {flow}")
+
+    # when
+    result12 = run_sequential_local(flow12, dispatcher)
+
+    # then
+    EXPECTED12 = 12
+    assert result12 == EXPECTED12
+
+    # when
+    result101 = run_sequential_local(flow101, dispatcher)
+
+    # then
+    EXPECTED101 = 101
+    assert result101 == EXPECTED101
